52c5c0d8cdb1cdb4bdffff528f7a641c6f41e8c4
Alejandro Rosa <halejandrorosa@gmail.com>
2024-02-15 17:55:57 -0300

    Actualización sesionContext.js

--- context/sessionContext.js
+++ context/sessionContext.js
@@ -1,11 +1,36 @@
 "use client"
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import jwtDecode from 'jwt-decode';
 
 const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
+  const router = useRouter();
   const [session, setSession] = useState(null);
+
+  // Al montar el componente, verificar si el token ha expirado y redirigir si es necesario
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      try {
+        const decodedToken = jwtDecode(accessToken);
+        if (decodedToken.exp < Date.now() / 1000) {
+          // Token expirado, redirigir a la página de inicio de sesión
+          router.push('/login');
+        } else {
+          // Establecer la sesión si el token no ha expirado
+          setSession(decodedToken);
+        }
+      } catch (error) {
+        console.error('Error al decodificar el token:', error);
+      }
+    } else {
+      // No hay token, redirigir a la página de inicio de sesión
+      router.push('/login');
+    }
+  }, [router]);
 
   return (
     <SessionContext.Provider value={{ session, setSession }}>
