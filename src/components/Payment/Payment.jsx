8510ea455ea411ed31cd051de1adc808940e9bea
Alejandro Rosa <halejandrorosa@gmail.com>
2024-02-21 13:18:07 -0300

    Editado Payment según tipo de cliente

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -4,11 +4,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { BounceLoader } from "react-spinners";
+import { Checkbox } from "antd";
 
 const Payment = () => {
   const [fecha] = useState(new Date());
   const [userInfo, setUserInfo] = useState({});
   const [loading, setLoading] = useState(true);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showPaymentOptions, setShowPaymentOptions] = useState(false);
+  const onChange = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+    if (e.target.value === "PAGO TIC") {
+      setShowPaymentOptions(true);
+    } else {
+      setShowPaymentOptions(false);
+    }
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -65,22 +80,50 @@ const Payment = () => {
               ${userInfo.saldo}
             </p>
           </div>
-          <div className="my-4 w-11/12 rounded-md border-2 bg-white p-2 lg:w-3/5">
-            <h3 className="p-2 text-2xl font-medium text-[#3184e4]">
-              ¿Cuánto deseas pagar?
-            </h3>
-            <hr className="m-2 border" />
-            <div className="flex items-center justify-center p-2 text-2xl font-medium text-[#3184e4]">
-              <p className="mr-2">$</p>
-              <input
-                type="number"
-                name="monto"
-                placeholder="Ingresá el monto"
-                required
-                className="my-2 flex lg:w-1/4 justify-center rounded-md bg-gray-100 py-2 placeholder:px-2 focus:border-[#3184e4] focus:outline-none focus:ring-4 focus:ring-[#3184e4]"
-              ></input>
+          {userInfo.tipoCliente === "1" ? (
+            <div className="my-4 w-11/12 rounded-md border-2 bg-white p-2 lg:w-3/5">
+              <h3 className="p-2 text-2xl font-medium text-[#3184e4]">
+                ¿Cuánto deseas pagar?
+              </h3>
+              <hr className="m-2 border" />
+              <div className="flex items-center justify-center p-2 text-2xl font-medium text-[#3184e4]">
+                <p className="mr-2">$</p>
+                <input
+                  type="number"
+                  name="monto"
+                  placeholder="Ingresá el monto"
+                  required
+                  className="my-2 flex justify-center rounded-md bg-gray-100 py-2 placeholder:px-2 focus:border-[#3184e4] focus:outline-none focus:ring-4 focus:ring-[#3184e4] lg:w-1/4"
+                />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="my-4 w-11/12 rounded-md border-2 bg-white p-2 lg:w-3/5">
+              <h3 className="p-2 text-2xl font-medium text-[#3184e4]">
+                ¿Qué deseas pagar?
+              </h3>
+              <hr className="m-2 border" />
+              {userInfo.comprobanteDeudaDTOS.map((comprobante, index) => (
+                <tr
+                  className="flex items-center justify-end text-xs"
+                  key={index}
+                >
+                  <td className="py-4 pl-2 font-medium lg:pr-60">
+                    <Checkbox onChange={onChange} className="mr-2"></Checkbox>
+                    {comprobante.documento}
+                  </td>
+                  <td className="text-grey-500 py-4 font-medium lg:pr-60">
+                    {comprobante.fecha}
+                  </td>
+                  <td className="py-4 pl-2 font-bold text-[#3184e4] lg:pr-60">
+                    ${comprobante.saldo}
+                  </td>
+                </tr>
+              ))}
+              <hr className="m-2 border" />
+              <p>Total a pagar</p>
+            </div>
+          )}
           <div className="my-4 w-11/12 rounded-md border-2 bg-white p-2 lg:w-3/5">
             <h3 className="p-2 text-2xl font-medium text-[#3184e4]">
               Forma de pago
@@ -90,26 +133,53 @@ const Payment = () => {
               <div className="p-2">
                 <input
                   type="radio"
-                  id="huey"
-                  name="drone"
-                  value="huey"
+                  id="pago-tic"
+                  name="payment"
+                  value="PAGO TIC"
                   className="mr-2"
+                  onChange={handleOptionChange}
+                  checked={selectedOption === "PAGO TIC"}
                 />
-                <label for="huey">PAGO TIC</label>
+                <label htmlFor="pago-tic">PAGO TIC</label>
               </div>
               <div className="p-2">
                 <input
                   type="radio"
-                  id="huey"
-                  name="drone"
-                  value="huey"
+                  id="mercado-pago"
+                  name="paymentOption"
+                  value="MERCADO PAGO"
                   className="mr-2"
+                  onChange={handleOptionChange}
+                  checked={selectedOption === "MERCADO PAGO"}
                 />
-                <label for="huey">MERCADO PAGO</label>
+                <label htmlFor="mercado-pago">MERCADO PAGO</label>
               </div>
             </div>
           </div>
-          <div className="my-4 w-11/12 rounded-md border-2 bg-white p-4 lg:w-3/5">
+          <div className="my-4 flex w-11/12 flex-col items-center rounded-md border-2 bg-white p-4 lg:w-3/5">
+            {showPaymentOptions && (
+              <div className="my-4 w-11/12 rounded-md border-2 bg-white p-2 ">
+                <h3 className="flex justify-center p-2 text-2xl font-medium text-[#3184e4]">
+                  Método de pago
+                </h3>
+                <hr className="m-2 border" />
+                <div className="flex justify-center font-medium text-[#3184e4]">
+                  {userInfo.formaPagos.map((option) => (
+                    <div className="p-2" key={option}>
+                      <input
+                        type="radio"
+                        id={option}
+                        name="paymentOption"
+                        value={option.toUpperCase()}
+                        className="mr-2"
+                        onChange={(e) => console.log(e.target.value)}
+                      />
+                      <label htmlFor={option}>{option.toUpperCase()}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
             <button className="my-2 flex w-full justify-center rounded-sm bg-[#3184e4] p-2 font-semibold text-white hover:bg-[#00478a]">
               <Link href="/friocalor/solicitar">PAGAR</Link>
             </button>
